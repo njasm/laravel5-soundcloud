9ba2cf5667f52d5c4f09ef48fe1538d11a32cceb
Nelson J Morais <njmorais@proactive-hosters.com>
2015-02-11 20:35:55 +0000

    init commit.

---
+++ src/SoundcloudProvider.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Njasm\Laravel\Soundcloud;
+
+use Illuminate\Support\ServiceProvider;
+
+class SoundcloudProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('Riak\Contracts\Connection', function($app)
+        {
+            return new Connection($app['config']['riak']);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['Riak\Contracts\Connection'];
+    }
+}
+
+
+<?php namespace App\Providers;
+
+use Riak\Connection;
+
+
+class RiakServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('Riak\Contracts\Connection', function($app)
+        {
+            return new Connection($app['config']['riak']);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['Riak\Contracts\Connection'];
+    }
+
+}
